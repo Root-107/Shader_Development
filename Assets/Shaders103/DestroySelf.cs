fed908f28bb5b2c235d91efaad0590093cb8e1e2
SPugh_107 <sam.pugh@dbpixelhouse.com>
2020-03-13 10:40:34 +0000

    creating test shaders, combing properties. Going over old stuff, refresher things and having examples noted down.

---
+++ Assets/Shaders103/DestroySelf.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Destroy());
+	}
+	
+	// Update is called once per frame
+	IEnumerator Destroy(){
+		yield return new WaitForSeconds(2.5f);
+		Destroy(gameObject);
+	}
+}
